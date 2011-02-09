45cd1461459ea01c5b55c0648f8f97f3d0d6c100
lordbulb <miroslavm@gmail.com>
2011-02-09 13:45:27 +0100

    changed the tests in Game and added junit library

--- Abalone/src/model/Game.java
+++ Abalone/src/model/Game.java
@@ -31,6 +31,17 @@ public class Game {
 		return board;
 	}
 
+	public TreeSet<Position> get_selected_positions(){
+		return selected_positions;
+	}
+	
+	public Player get_current_player(){
+		return players.get_current();
+	}
+	
+	public int get_pieces_to_win(Player p){
+		return pieces_to_win.get(p).intValue();
+	}
 	
 	public boolean check_move(Player p, Move m){
 		Map<Position, Position> ownpos= m.get_own_positions();
@@ -196,81 +207,33 @@ public class Game {
         }
 	
 	public static void main(String[] args){
-		Ring<Player> r = new Ring<Player>();
-//		System.out.println(r.get_current());
-		r.add(new Player(1, "test1"));
-//		System.out.println("111"+r.get_current());
-		r.add(new Player(1, "test2"));
-//		System.out.println("222"+r.get_current());
-		Game g = new Game(r);
-//		Move m = new Move(1);
 		
-		System.out.println(g.add_position(new Position(4,2)));
-		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(5,3)));
-		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(6,4)));
-		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(3,2)));
-		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(3,3)));
-		System.out.println(g.selected_positions);
-                System.out.println(g.add_position(new Position(4,2)));
-		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(3,4)));
-		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(9,8)));
-		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(8,8)));
-		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(7,8)));
-		System.out.println(g.selected_positions);
+		Ring<Player> r = new Ring<Player>();
+        r.add(new Player(1, "test1"));
+        r.add(new Player(1, "test2"));
+        Game g = new Game(r);
+
+        System.out.println(g.add_position(new Position(1,4)));
+		System.out.println(g.get_selected_positions());
 		System.out.println(g.add_position(new Position(1,3)));
-		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(1,5)));
-		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(1,4)));
-		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(2,1)));
-		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(2,2)));
-		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(2,4)));
-		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(3,5)));
-		System.out.println(g.selected_positions);
-//		System.out.println(g.add_position(new Position(2,5)));
-//		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(1,4)));
-		System.out.println(g.selected_positions);
-		System.out.println(g.add_position(new Position(1,3)));
-		System.out.println(g.selected_positions);
+		System.out.println(g.get_selected_positions());
 		System.out.println(g.add_position(new Position(1,2)));
-		System.out.println(g.selected_positions);
-//                System.out.println(g.add_position(new Position(3,3)));
-//		System.out.println(g.selected_positions);
-//		System.out.println(g.add_position(new Position(2,3)));
-//		System.out.println(g.selected_positions);
-//		System.out.println(g.add_position(new Position(1,3)));
-//                System.out.println(g.selected_positions);
-                Position p11 = new Position(1,1);
-                Move m = g.gen_move(p11);
-                System.out.println(m.get_own_positions());
-                System.out.println(m.get_other_positions());
-                System.out.println(m.get_direction());
-                System.out.println(m.is_parallel());
-//                System.out.println(g.check_neighbour_position(new Position(1,2)));
-//                System.out.println(g.gen_move(new Position(4,4)).get_own_positions());
-                System.out.println(g.check_position_for_move(p11, m));
-                g.move(m);
-                System.out.println(g.board.get_fields().get(new Position(1,4)).is_empty());
-//                System.out.println(new Move(g.selected_positions,1).get_own_positions());
-//		m.add_position(new Position(5,5));
-//		System.out.println(g.get_players().get_current());
-//		System.out.println(g.check_move(g.get_players().get_current(), m));
-//		System.out.println(g.add_position(new Position(3,3)));
-//		System.out.println(g.add_position(new Position(3,5)));
+		System.out.println(g.get_selected_positions());
+
+	    Position p11 = new Position(1,1);
+	    Move m = g.gen_move(p11);
+	            
+	    System.out.println(m.get_own_positions());
+	    System.out.println(m.get_other_positions());
+	    System.out.println(m.get_direction());
+	    System.out.println(m.is_parallel());
+	    System.out.println(g.check_position_for_move(p11, m));
+	            
+	    g.move(m);
+	            
+	    System.out.println(g.get_board().get_fields().get(new Position(1,4)).is_empty());
+	    System.out.println(g.get_pieces_to_win(g.get_current_player()));
+	            
 	}
-	
 	
 }
