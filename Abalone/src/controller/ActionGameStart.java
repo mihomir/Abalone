68efb2eadba5f38c9b8f1ab457df2797a86b995c
veronichkad <veronichkad@gmail.com>
2011-02-11 15:11:08 +0100

    added popup with check option for new game

--- Abalone/src/controller/ActionGameStart.java
+++ Abalone/src/controller/ActionGameStart.java
@@ -7,13 +7,17 @@ import javax.swing.*;
 import java.util.*;
 public class ActionGameStart extends AbstractAction{
 	DrawMain dm;
-	public ActionGameStart(String name, DrawMain drawm){		
+	JDialog jd;
+	public ActionGameStart(String name, DrawMain drawm, JDialog dialog){		
 		super();
 		dm=drawm;
+		jd= dialog;
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		
+		jd.setVisible(false);
 		Player p1 = new Player(1,"test1");
 		Player p2 = new Player(1,"test2");
 		Ring<Player> lp = new Ring<Player>();
